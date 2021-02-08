1e9e4f1164dbfb8bcbae35266339d7df6fde5d29
rhtm123 <maurya.iitk@gmail.com>
2021-02-08 16:09:52 +0530

    V 0.0.4

--- datascience_project_website/heroku_settings.py
+++ datascience_project_website/heroku_settings.py
@@ -32,7 +32,7 @@ def find_or_create_secret_key():
 ALLOWED_HOSTS = ['dsmlapp.herokuapp.com',
                  'www.datascienceprojects.info', 'datascienceprojects.info']
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = find_or_create_secret_key()
 
--- datascience_project_website/settings.py
+++ datascience_project_website/settings.py
@@ -47,7 +47,7 @@ def find_or_create_secret_key():
 SECRET_KEY = find_or_create_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
